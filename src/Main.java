d97d75d649564f4e0957078ea046d22443b1417e
unknown <bt_1e10_19@bktech.local>
2022-02-04 23:42:51 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        ArrayList<Dog> dogList = new ArrayList<Dog>();
+        Dog dog1 = new Dog("Spot");
+        Dog dog2 = new Dog("Benny");
+        dogList.add(dog1);
+        dogList.add(dog2);
+        dogList.get(0).bark();
 
+
+
+    }
 }
+
